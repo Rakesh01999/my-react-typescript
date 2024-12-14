82be7db3b483cb1a87dfb4fbe9f729af859f36c5
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-14 14:04:20 +0600

    unused svg removed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -109,21 +109,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               fill=""
             />
           </svg>
-
-          <svg
-            className="fill-current text-gray-600"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M3 6h18M3 12h18M3 18h18"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
         </button>
       </div>
       {/* <!-- SIDEBAR HEADER --> */}
