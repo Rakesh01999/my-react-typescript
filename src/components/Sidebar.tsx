42d3d810ede78f3f2c841f7e1fa9a16d4cdd7dd6
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-23 21:41:42 +0600

    added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -121,7 +121,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           {/* <!-- Menu Group --> */}
           <div>
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
-              MENU Here
+              MENU
             </h3>
 
             <ul className="mb-6 flex flex-col gap-1.5">
