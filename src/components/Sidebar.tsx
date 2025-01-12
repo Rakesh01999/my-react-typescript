5cbbd9d051894f3bfe32703dacb7a22ecab6d696
Rakesh Biswas <rbiswas01999@gmail.com>
2025-01-12 22:04:30 +0600

    changes added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -121,10 +121,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
         <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
           {/* <!-- Menu Group --> */}
           <div>
+            
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
               MENU Here
             </h3>
-
             <ul className="mb-6 flex flex-col gap-1.5">
               {/* <!-- Menu Item Dashboard --> */}
               <li>
